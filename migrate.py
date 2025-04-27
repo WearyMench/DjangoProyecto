44984275cf3805909da5fe9a4fa671b2697b352d
Adrian Mirabal <adrian.mirabal07@gmail.com>
2025-04-27 17:13:04 -0400

    update

--- migrate.py
+++ migrate.py
@@ -6,7 +6,12 @@ from django.core.management import execute_from_command_line
 def run_migrations():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyWeb.settings')
     django.setup()
+    
+    # Run migrations
     execute_from_command_line(['manage.py', 'migrate'])
+    
+    # Collect static files
+    execute_from_command_line(['manage.py', 'collectstatic', '--noinput'])
 
 if __name__ == '__main__':
     run_migrations() 
